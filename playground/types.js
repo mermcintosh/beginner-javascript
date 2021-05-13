ef4028f8b3d110c3339a77db31652528bbd31e5a
Meredith <meredith@mcintosh.io>
2021-05-13 15:38:56 -0400

    null and undefined

--- playground/types.js
+++ playground/types.js
@@ -20,3 +20,28 @@ const person = {
     last: 'mcintosh',
     age: 28
 };
+
+//null
+//null is a value of nothing
+//literally has a value of null
+const somethingNull = null;
+
+//null example below
+//Teller is a magician who legally changed his name to only be Teller
+//so in this example, he values his lastname to null because he doesn't have one
+//where for example Cher might have a last name but we just don't know it,
+//so her name could return undefined
+const teller = {
+    first: 'Raymond',
+    last: 'Teller'
+}
+
+teller.first = "Teller";
+teller.last = null
+
+
+//undefined
+//undefined comes about when you try to access a variable
+//and it hasn't been set
+let dog;
+console.log(dog);
