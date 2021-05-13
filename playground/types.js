93ea90b5b133f5f71360d88382f93609c5421d15
Meredith <meredith@mcintosh.io>
2021-05-13 11:40:42 -0400

    types

---
+++ playground/types.js
@@ -0,0 +1,22 @@
+const name = 'meredith'
+const middle = 'hannah'
+const last = 'mcintosh'
+
+{/* // SNOB'N'US or SNOBUSN
+//string
+//number
+//boolean
+//null
+//undefined
+//symbol
+//object */}
+
+//this is an object or collection
+//order doesn't matter in an object
+// use . notation o be able to see specific data
+// person.first will return 'meredith'
+const person = {
+    first: 'meredith',
+    last: 'mcintosh',
+    age: 28
+};
