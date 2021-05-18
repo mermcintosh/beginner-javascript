2fe7d272d3a0c357cb3a766452dbeff0e19573b2
Meredith <meredith@mcintosh.io>
2021-05-18 15:33:00 -0400

    select elements review

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,8 @@
+const p = document.querySelector('p')
+console.log(p)
+
+const divs = document.querySelectorAll('div')
+console.log(divs)
+
+//node list is not an array. it is a list, but it doesn't have all
+//the methods that arrays have
