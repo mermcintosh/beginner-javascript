7640a32643be4df589b81c10c5eab22a2baf080a
Meredith <meredith@mcintosh.io>
2021-05-18 14:31:27 -0400

    scope notes

---
+++ playground/scope.js
@@ -0,0 +1,15 @@
+const first = "wes"
+
+//global scope - variables can be used anywhere
+
+//function scope - variables can only be used in a function
+//look at the curly brackets with a function as a fence that keeps everything inside,
+//but it can let variable values leak in INTERESTING
+
+//you can name variables the same thing that are in different scopes
+// butttttt naming variables the same is not advised
+
+//block scope - curly brackets can be called a block
+//const variables can not be reassigned
+//let can be reassigned
+
