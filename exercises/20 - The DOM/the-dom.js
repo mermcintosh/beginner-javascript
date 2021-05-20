3a728ccf77881ecda0c1e5ad3d3d5ec62e746188
Meredith <meredith@mcintosh.io>
2021-05-20 10:52:08 -0400

    creating html notes

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -6,3 +6,11 @@ console.log(divs)
 
 //node list is not an array. it is a list, but it doesn't have all
 //the methods that arrays have
+
+//console.dir('h2') <-- will share all the info about
+//the object (the heading)
+
+
+
+//data attributes
+
